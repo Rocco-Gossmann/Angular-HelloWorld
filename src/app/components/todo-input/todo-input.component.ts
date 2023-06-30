57c6854c5201b892fd390ce73533d1c8a7e1fd0d
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-06-30 06:35:57 +0200

    Form-Inputs

--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
+import todoManager from 'projects/todos/src/data/todomanager/todomanager.module';
 
 @Component({
   selector: 'app-todo-input',
   templateUrl: './todo-input.component.html',
   styleUrls: ['./todo-input.component.css']
 })
-export class TodoInputComponent {
+export class TodoInputComponent implements OnChanges {
+
+  textInput: string = "";
+
+  ngOnChanges(changes: SimpleChanges): void {
+      throw new Error('Method not implemented.');
+  }
+
+  onInputSubmit() {
+    if(!this.textInput.trim()) return;
+
+    todoManager.list.add({
+        description: this.textInput,
+        done: false,
+        tags: []
+    });
+
+    this.textInput = "";
+  }
 
 }
