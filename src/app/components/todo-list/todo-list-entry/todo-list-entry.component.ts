c564c62c848aeac629a85196231eb4c47407532e
Rocco Goßmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2023-07-08 07:09:58 +0200

    animation via css seems the way to go

--- src/app/components/todo-list/todo-list-entry/todo-list-entry.component.ts
+++ src/app/components/todo-list/todo-list-entry/todo-list-entry.component.ts
@@ -1,36 +1,39 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core'
-import { trigger, state, style, transition, animate } from '@angular/animations';
-
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core'
 import { TodoData } from '../../../../data/todomanager/todomanager.module';
 
 @Component({
   selector: 'app-todo-list-entry',
   templateUrl: './todo-list-entry.component.html',
-  styleUrls: ['./todo-list-entry.component.css'],
-  animations: [
-    trigger("listElement", [
-      state("done", style({
-        backgroundColor: "lightgreen"
-      })),
-      state("undone", style({
-        backgroundColor: "coral"
-      })),
-
-      transition("done => undone", animate(".5s")),
-      transition("undone => done", animate(".5s"))
-    ])
-  ]
+  styleUrls: ['./todo-list-entry.component.css']
 })
 export class TodoListEntryComponent {
 
   @Input() todo?: TodoData
   @Output() onDone: EventEmitter<boolean> = new EventEmitter();
+  @ViewChild("maincontainer") container!: ElementRef<HTMLElement>;
+
+  animclass = "";
+
+  comeInAnimation = () => {
+    console.log("come in end");
+    this.container.nativeElement.removeEventListener("animationend", this.comeInAnimation);
+    this.animclass = ""
+  }
+  goOutAnimation = () => {
+    console.log("go out end");
+    this.container.nativeElement.removeEventListener("animationend", this.goOutAnimation);
+    this.container.nativeElement.addEventListener("animationend", this.comeInAnimation);
+    this.animclass = "animin"
+    this.onDone.emit(this.todo?.done);
+  }
 
   toggleDone() {
-    if(this.todo)
+    if(this.todo) {
       this.todo.done = !this.todo.done
+    }
 
-    this.onDone.emit(this.todo?.done);
+    this.container.nativeElement.addEventListener("animationend", this.goOutAnimation);
+    this.animclass = "animout"
   }
 
 }
